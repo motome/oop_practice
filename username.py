dd4bdbbe6c5afd69eb82e680a6b97e5a83ac64d7
motome <sigmapinefield15@gmail.com>
2023-09-24 17:17:21 +0900

    完成
    疲れた

---
+++ username.py
@@ -0,0 +1,14 @@
+class UserName:
+    def __init__(self, name):
+        if not (4 <= len(name) <= 20):
+            raise ValueError(f"{name}はルール違反だぞ！")
+            pass
+        self.name = name
+
+    def screen_name(self):
+       return self.name.upper()
+
+
+tanaka = UserName(name="tanaka")
+# print(tanaka.name)
+print(tanaka.screen_name())
